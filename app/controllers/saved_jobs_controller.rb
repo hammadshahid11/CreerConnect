0e140478923a0af16c36264187b0ec8d05825fe6
hammadshahid11 <hammad.shahid@recursolabs.com>
2023-09-22 20:03:21 +0500

    added unsave and save job seeker post

--- app/controllers/saved_jobs_controller.rb
+++ app/controllers/saved_jobs_controller.rb
@@ -12,17 +12,22 @@ class SavedJobsController < ApplicationController
     def new
       @job_seeker = JobSeeker.find(params[:job_seeker_id])
       @job_posting = JobPosting.find(params[:job_posting_id])
-  
-      # Create a new saved job record
-      @saved_job = SavedJob.new(job_seeker: @job_seeker, job_posting: @job_posting, date_saved: Time.now)
-  
-      if @saved_job.save!
-        
-        redirect_to job_seeker_saved_jobs_path(@job_seeker), notice: 'Job saved successfully.'
+    
+      # Check if the job has already been saved by the job seeker
+      existing_saved_job = SavedJob.find_by(job_seeker: @job_seeker, job_posting: @job_posting)
+    
+      if existing_saved_job
+        redirect_to job_seeker_saved_jobs_path(@job_seeker), alert: 'Job has already been saved.'
       else
-        redirect_to job_listing_path(@job_posting), alert: 'Job could not be saved.'
+        # Create a new saved job record
+        @saved_job = SavedJob.new(job_seeker: @job_seeker, job_posting: @job_posting, date_saved: Time.now)
+    
+        if @saved_job.save
+          redirect_to job_seeker_saved_jobs_path(@job_seeker), notice: 'Job saved successfully.'
+        else
+          redirect_to job_listing_path(@job_posting), alert: 'Job could not be saved.'
+        end
       end
-
     end
     
 
@@ -43,17 +48,22 @@ class SavedJobsController < ApplicationController
       end
     end
   
+   
+
     # Remove a saved job for a job seeker
     def destroy
-      debugger
       @job_seeker = JobSeeker.find(params[:job_seeker_id])
-      @saved_job = @job_seeker.saved_jobs.find(params[:id])
-  
-      if @saved_job.destroy
+      @job_posting = JobPosting.find(params[:id]) # Use params[:id] to find the job posting
+
+      # Find the saved job based on both job seeker and job posting
+      @saved_job = SavedJob.find_by(job_seeker: @job_seeker, job_posting: @job_posting)
+
+      if @saved_job && @saved_job.destroy
         redirect_to job_seeker_saved_jobs_path(@job_seeker), notice: 'Job removed from saved list.'
       else
         redirect_to job_seeker_saved_jobs_path(@job_seeker), alert: 'Unable to remove the job.'
       end
     end
+
   end
   
